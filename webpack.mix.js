0990bfb6a37a1712b782efa605d9cdca0708210b
RyotaMoriyama <info@ryotamoriyama.com>
2021-02-25 20:21:10 +0900

    MModify

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,5 @@
 const mix = require('laravel-mix')
 const fs = require('fs-extra')
-const glob = require('glob')
 const multimatch = require('multimatch')
 const SVGSpritemapPlugin = require('svg-spritemap-webpack-plugin')
 require('laravel-mix-polyfill')
@@ -10,120 +9,79 @@ require('laravel-mix-stylelint')
 require('laravel-mix-imagemin')
 require('laravel-mix-webp')
 
-const srcRelativePath = process.env.MIX_SRC_RELATIVE_PATH;
-const distRelativePath = process.env.MIX_DIST_RELATIVE_PATH;
-
+const srcRelativePath =
+  (process.env.MIX_SRC_RELATIVE_PATH || 'resources/themes/input-theme-name')
+    .replace(/\/$/, '')
+const distRelativePath =
+  (process.env.MIX_DIST_RELATIVE_PATH || 'wp/wp-content/themes/input-theme-name')
+    .replace(/\/$/, '')
 
 fs.removeSync(`${distRelativePath}/assets`)
 
 mix
-    .setPublicPath(distRelativePath)
-    .polyfill()
-    .js(
-        `${srcRelativePath}/assets/js/vendor.js`,
-        `${distRelativePath}/assets/js`
-    )
-    .js(
-        `${srcRelativePath}/assets/js/vimeo-player.js`,
-        `${distRelativePath}/assets/js`
-    )
-    .js(
-        `${srcRelativePath}/assets/js/common.js`,
-        `${distRelativePath}/assets/js`
-    )
-    .js(
-        `${srcRelativePath}/assets/js/index.js`,
-        `${distRelativePath}/assets/js`
-    )
-    .js(
-        `${srcRelativePath}/assets/js/about.js`,
-        `${distRelativePath}/assets/js`
-    )
-    .js(
-        `${srcRelativePath}/assets/js/archive-gallery.js`,
-        `${distRelativePath}/assets/js`
-    )
-    .js(
-        `${srcRelativePath}/assets/js/archive-news.js`,
-        `${distRelativePath}/assets/js`
-    )
-    .js(
-        `${srcRelativePath}/assets/js/single-news.js`,
-        `${distRelativePath}/assets/js`
-    )
-    .js(
-        `${srcRelativePath}/assets/js/notfound.js`,
-        `${distRelativePath}/assets/js`
-    )
-    .eslint()
-    .sass(
-        `${srcRelativePath}/assets/css/app.scss`,
-        `${distRelativePath}/assets/css`
-    )
-    .stylelint({context: srcRelativePath})
-    .options({processCssUrls: false})
-    .webpackConfig({
-        plugins: [
-            new SVGSpritemapPlugin(
-                `${srcRelativePath}/assets/svg/sprite/*.svg`,
-                {
-                    output: {
-                        filename: 'assets/svg/sprite.svg',
-                        chunk: {
-                            name: 'assets/js/.svg-dummy-module',
-                            keep: true
-                        },
-                        svgo: {
-                            plugins: [
-                                {removeTitle: true},
-                                {addAttributesToSVGElement: {attributes: [{'data-svg-sprite': ''}]}}
-                            ]
-                        },
-                        svg4everybody: true
-                    }
-                }
-            )
-        ],
-        module: {
-            rules: [
-                {
-                    test: /\.scss/,
-                    loader: 'import-glob-loader'
-                }
-            ]
+  .setPublicPath(distRelativePath)
+  .polyfill()
+  .js(
+    `${srcRelativePath}/assets/js/app.js`,
+    `${distRelativePath}/assets/js`
+  )
+  .eslint()
+  .sass(
+    `${srcRelativePath}/assets/css/app.scss`,
+    `${distRelativePath}/assets/css`
+  )
+  .stylelint({ context: srcRelativePath })
+  .options({ processCssUrls: false })
+  .webpackConfig({
+    plugins: [
+      new SVGSpritemapPlugin(
+        `${srcRelativePath}/assets/svg/sprite/*.svg`,
+        {
+          output: {
+            filename: 'assets/svg/sprite.svg',
+            chunk: {
+              name: 'assets/js/.svg-dummy-module',
+              keep: true
+            },
+            svgo: {
+              plugins: [
+                { addAttributesToSVGElement: { attributes: [{ 'data-svg-sprite': '' }] } }
+              ]
+            },
+            svg4everybody: true
+          }
         }
-    })
-    .copyWatched(
-        [
-            `${srcRelativePath}/assets/svg/!(sprite)`,
-            `${srcRelativePath}/assets/svg/!(sprite)/**/*`
-        ],
-        `${distRelativePath}/assets/svg`,
-        {base: `${srcRelativePath}/assets/svg`}
-    )
-    .copyWatched([`${srcRelativePath}/assets/lib`], `${distRelativePath}/assets/lib`, {
-        base: `${srcRelativePath}/assets/lib`,
-    })
-    .browserSync({
-        open: false,
-        host: process.env.MIX_BROWSER_SYNC_HOST || 'localhost',
-        port: process.env.MIX_BROWSER_SYNC_PORT || 3000,
-        proxy: process.env.MIX_BROWSER_SYNC_PROXY || false,
-        files: [
-            `${distRelativePath}/assets/**/*`,
-            `${distRelativePath}/**/*.php`,
-            `!${distRelativePath}/functions/autoload/99_acf_setting.php`
-        ],
-        https:
-            process.env.MIX_BROWSER_SYNC_HTTPS_CERT &&
-            process.env.MIX_BROWSER_SYNC_HTTPS_KEY
-                ? {
-                    cert: process.env.MIX_BROWSER_SYNC_HTTPS_CERT,
-                    key: process.env.MIX_BROWSER_SYNC_HTTPS_KEY
-                }
-                : false
-    })
-    .sourceMaps(false, 'inline-cheap-module-source-map')
+      )
+    ]
+  })
+  .copyWatched(
+    [
+      `${srcRelativePath}/assets/svg/!(sprite)`,
+      `${srcRelativePath}/assets/svg/!(sprite)/**/*`
+    ],
+    `${distRelativePath}/assets/svg`,
+    { base: `${srcRelativePath}/assets/svg` }
+  )
+  .browserSync({
+    open: false,
+    host: process.env.MIX_BROWSER_SYNC_HOST || 'localhost',
+    port: process.env.MIX_BROWSER_SYNC_PORT || 3000,
+    proxy: process.env.MIX_BROWSER_SYNC_PROXY || false,
+    files: [
+      `${distRelativePath}/assets/**/*`,
+      `${distRelativePath}/**/*.php`,
+      `!${distRelativePath}/functions/autoload/99_acf_setting.php`
+    ],
+    https:
+      process.env.MIX_BROWSER_SYNC_HTTPS_CERT &&
+      process.env.MIX_BROWSER_SYNC_HTTPS_KEY
+        ? {
+          cert: process.env.MIX_BROWSER_SYNC_HTTPS_CERT,
+          key: process.env.MIX_BROWSER_SYNC_HTTPS_KEY
+        }
+        : false
+  })
+  .sourceMaps(false, 'inline-cheap-module-source-map')
 
 if (process.env.NODE_ENV === 'production') {
     mix
@@ -158,10 +116,10 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 else {
-    mix
-        .copyWatched(
-            `${srcRelativePath}/assets/images`,
-            `${distRelativePath}/assets/images`,
-            {base: `${srcRelativePath}/assets/images`}
-        )
+  mix
+    .copyWatched(
+      `${srcRelativePath}/assets/images`,
+      `${distRelativePath}/assets/images`,
+      { base: `${srcRelativePath}/assets/images` }
+    )
 }
