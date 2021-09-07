7b54e879152278d2adc2caea1060baab85880bd4
ryota moriyama <info@ryotamoriyama.com>
2021-09-07 17:10:37 +0900

    Modify conflict

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,53 +1,39 @@
-const mix = require('laravel-mix')
-const fs = require('fs-extra')
-const multimatch = require('multimatch')
-const SVGSpritemapPlugin = require('svg-spritemap-webpack-plugin')
-require('laravel-mix-polyfill')
-require('laravel-mix-copy-watched')
-require('laravel-mix-eslint')
-require('laravel-mix-stylelint')
-require('laravel-mix-imagemin')
-require('laravel-mix-webp')
+const mix = require("laravel-mix");
+const fs = require("fs-extra");
+const multimatch = require("multimatch");
+const SVGSpritemapPlugin = require("svg-spritemap-webpack-plugin");
+require("laravel-mix-polyfill");
+require("laravel-mix-copy-watched");
+require("laravel-mix-eslint");
+require("laravel-mix-stylelint");
+require("laravel-mix-imagemin");
+require("laravel-mix-webp");
 
-const srcRelativePath =
-  (process.env.MIX_SRC_RELATIVE_PATH || 'resources/themes/input-theme-name')
-    .replace(/\/$/, '')
-const distRelativePath =
-  (process.env.MIX_DIST_RELATIVE_PATH || 'wp/wp-content/themes/input-theme-name')
-    .replace(/\/$/, '')
+const srcRelativePath = (
+  process.env.MIX_SRC_RELATIVE_PATH || "resources/themes/input-theme-name"
+).replace(/\/$/, "");
+const distRelativePath = (
+  process.env.MIX_DIST_RELATIVE_PATH || "wp/wp-content/themes/input-theme-name"
+).replace(/\/$/, "");
 
-fs.removeSync(`${distRelativePath}/assets`)
+fs.removeSync(`${distRelativePath}/assets`);
 
 mix
   .setPublicPath(distRelativePath)
   .polyfill()
-  .js(
-    `${srcRelativePath}/assets/js/app.js`,
-    `${distRelativePath}/assets/js`
-  )
+  .js(`${srcRelativePath}/assets/js/app.js`, `${distRelativePath}/assets/js`)
   .eslint()
   .webpackConfig({
-<<<<<<< HEAD
-       module: {
-           rules: [{
-               test: /\.scss/,
-               enforce: 'pre',
-               loader: 'import-glob-loader'
-           }]
-       }
-   })
-=======
     module: {
       rules: [
         {
           test: /\.scss/,
-          enforce: 'pre',
-          loader: 'import-glob-loader'
-        }
-      ]
-    }
+          enforce: "pre",
+          loader: "import-glob-loader",
+        },
+      ],
+    },
   })
->>>>>>> 4a6ba1f0fb7680e235cc4ede3a27c3bf8e335295
   .sass(
     `${srcRelativePath}/assets/css/app.scss`,
     `${distRelativePath}/assets/css`
@@ -56,87 +42,90 @@ mix
   .options({ processCssUrls: false })
   .webpackConfig({
     plugins: [
-      new SVGSpritemapPlugin(
-        `${srcRelativePath}/assets/svg/sprite/*.svg`,
-        {
-          output: {
-            filename: 'assets/svg/sprite.svg',
-            chunk: {
-              name: 'assets/js/.svg-dummy-module',
-              keep: true
-            },
-            svgo: {
-              plugins: [
-                { addAttributesToSVGElement: { attributes: [{ 'data-svg-sprite': '' }] } }
-              ]
-            },
-            svg4everybody: true
-          }
-        }
-      )
-    ]
+      new SVGSpritemapPlugin(`${srcRelativePath}/assets/svg/sprite/*.svg`, {
+        output: {
+          filename: "assets/svg/sprite.svg",
+          chunk: {
+            name: "assets/js/.svg-dummy-module",
+            keep: true,
+          },
+          svgo: {
+            plugins: [
+              {
+                addAttributesToSVGElement: {
+                  attributes: [{ "data-svg-sprite": "" }],
+                },
+              },
+            ],
+          },
+          svg4everybody: true,
+        },
+      }),
+    ],
   })
   .copyWatched(
     [
       `${srcRelativePath}/assets/svg/!(sprite)`,
-      `${srcRelativePath}/assets/svg/!(sprite)/**/*`
+      `${srcRelativePath}/assets/svg/!(sprite)/**/*`,
     ],
     `${distRelativePath}/assets/svg`,
     { base: `${srcRelativePath}/assets/svg` }
   )
   .browserSync({
     open: false,
-    host: process.env.MIX_BROWSER_SYNC_HOST || 'localhost',
+    host: process.env.MIX_BROWSER_SYNC_HOST || "localhost",
     port: process.env.MIX_BROWSER_SYNC_PORT || 3000,
     proxy: process.env.MIX_BROWSER_SYNC_PROXY || false,
     files: [
       `${distRelativePath}/assets/**/*`,
       `${distRelativePath}/**/*.php`,
-      `!${distRelativePath}/functions/autoload/99_acf_setting.php`
+      `!${distRelativePath}/functions/autoload/99_acf_setting.php`,
     ],
     https:
       process.env.MIX_BROWSER_SYNC_HTTPS_CERT &&
       process.env.MIX_BROWSER_SYNC_HTTPS_KEY
         ? {
-          cert: process.env.MIX_BROWSER_SYNC_HTTPS_CERT,
-          key: process.env.MIX_BROWSER_SYNC_HTTPS_KEY
-        }
-        : false
+            cert: process.env.MIX_BROWSER_SYNC_HTTPS_CERT,
+            key: process.env.MIX_BROWSER_SYNC_HTTPS_KEY,
+          }
+        : false,
   })
-  .sourceMaps(false, 'inline-cheap-module-source-map')
+  .sourceMaps(false, "inline-cheap-module-source-map");
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   mix
     .version()
     .imagemin(
-      [ 'assets/images/**/*' ],
+      ["assets/images/**/*"],
       { context: srcRelativePath },
       {
-        test: filePath => !!multimatch(filePath, [ 'assets/images/**/*' ]).length,
-        pngquant: { strip: true, quality: 100-100 }, // 0 ~ 100
+        test: (filePath) =>
+          !!multimatch(filePath, ["assets/images/**/*"]).length,
+        pngquant: { strip: true, quality: 100 - 100 }, // 0 ~ 100
         gifsicle: { optimizationLevel: 1 }, // 1 ~ 3
-        plugins: [ require('imagemin-mozjpeg')({ quality: 100 }) ] // 0 ~ 100
+        plugins: [require("imagemin-mozjpeg")({ quality: 100 })], // 0 ~ 100
       }
     )
     .then(() => {
-      const svgDummyModuleName = 'assets/js/.svg-dummy-module'
-      fs.removeSync(`${distRelativePath}/${svgDummyModuleName}.js`)
-      const pathToManifest = `${distRelativePath}/mix-manifest.json`
-      const manifest = require(`./${pathToManifest}`)
-      delete manifest[`/${svgDummyModuleName}.js`]
-      fs.writeFileSync(path.resolve(pathToManifest), JSON.stringify(manifest), 'utf-8')
+      const svgDummyModuleName = "assets/js/.svg-dummy-module";
+      fs.removeSync(`${distRelativePath}/${svgDummyModuleName}.js`);
+      const pathToManifest = `${distRelativePath}/mix-manifest.json`;
+      const manifest = require(`./${pathToManifest}`);
+      delete manifest[`/${svgDummyModuleName}.js`];
+      fs.writeFileSync(
+        path.resolve(pathToManifest),
+        JSON.stringify(manifest),
+        "utf-8"
+      );
     })
     .ImageWebp({
       from: `${srcRelativePath}/assets/images`,
       to: `${distRelativePath}/assets/images`,
-    })
-}
-
-else {
-  mix
-    .copyWatched(
-      `${srcRelativePath}/assets/images`,
-      `${distRelativePath}/assets/images`,
-      { base: `${srcRelativePath}/assets/images` }
-    )
+    });
+} else {
+  mix.copyWatched(
+    `${srcRelativePath}/assets/images`,
+    `${distRelativePath}/assets/images`,
+    { base: `${srcRelativePath}/assets/images` }
+  );
 }
